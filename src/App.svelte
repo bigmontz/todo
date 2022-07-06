c3be2c68ac9a5c890413da1c32a0dc52d3272e8c
Antonio Barcélos <antonio.barcelos@neo4j.com>
2022-07-06 15:10:22 -0300

    Create First Page (#2)

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,13 @@
 <script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  import logo from './assets/todo-icon.svg'
+  import New from './lib/New.svelte'
 </script>
 
 <main>
   <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
+  <h1>Yet Another Todo List</h1>
 
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <New/>
 </main>
 
 <style>
@@ -38,7 +28,7 @@
   }
 
   h1 {
-    color: #ff3e00;
+    color: navy;
     text-transform: uppercase;
     font-size: 4rem;
     font-weight: 100;
@@ -47,18 +37,8 @@
     max-width: 14rem;
   }
 
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
   @media (min-width: 480px) {
     h1 {
-      max-width: none;
-    }
-
-    p {
       max-width: none;
     }
   }
