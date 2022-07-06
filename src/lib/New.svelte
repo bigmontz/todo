5773a90d60c791dde73175da7f67906d683fbba1
Antonio Barcelos <antonio.barcelos@neo4j.com>
2022-07-06 20:09:42 +0200

    Create First Page

---
+++ src/lib/New.svelte
@@ -0,0 +1,33 @@
+<script>
+  let slug = ""
+  const handleSubmit = () => {
+    window.location.assign(`${slug}`)
+  }
+</script>
+
+
+<form on:submit|preventDefault="{handleSubmit}">
+  <input placeholder="your-todo-list-name" name="name" bind:value="{slug}" type="text" required/>
+  <button type="submit">Go!</button>
+</form>
+
+<style>
+
+  button, input {
+    color: black;
+    font-size: 2rem;
+    font-weight: 100;
+    line-height: 1.1;
+    margin: 1rem auto;
+  }
+
+  input {
+    min-width: 10rem;
+    max-width: 40rem;
+  }
+  
+  @media (min-width: 480px) {
+    
+  }
+</style>
+
